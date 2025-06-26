24f98d57535870c0b8d017e49cda6bb858f02b51
Esteban Cristóbal <ecristobalr@gmail.com>
2025-06-26 21:59:48 +0200

    feat(infrastructure): Added Confluent Cloud Api key management

--- infrastructure/terraform/workspaces/main.tf
+++ infrastructure/terraform/workspaces/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/tfe"
       version = "0.67.0"
     }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "2.23.0"
+    }
   }
 }
 
@@ -26,6 +30,38 @@ resource "tfe_workspace" "confluent-environment" {
   }
 }
 
-resource "tfe_workspace_settings" "confluent-environment" {
+data "confluent_service_account" "automator" {
+  id = "sa-1223xpv"
+}
+
+resource "confluent_api_key" "automator" {
+  display_name = "Terraform - Environment"
+  description  = "API key for Automator service account to use in Terraform Cloud"
+
+  owner {
+    id          = data.confluent_service_account.automator.id
+    api_version = data.confluent_service_account.automator.api_version
+    kind        = data.confluent_service_account.automator.kind
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "tfe_variable" "confluent-api-key" {
+  key          = "CONFLUENT_CLOUD_API_KEY"
+  value        = confluent_api_key.automator.id
+  category     = "env"
+  description  = "Confluent Cloud API key"
+  workspace_id = tfe_workspace.confluent-environment.id
+}
+
+resource "tfe_variable" "confluent-api-secret" {
+  key          = "CONFLUENT_CLOUD_API_SECRET"
+  value        = confluent_api_key.automator.secret
+  category     = "env"
+  sensitive    = true
+  description  = "Confluent Cloud API secret"
   workspace_id = tfe_workspace.confluent-environment.id
 }
