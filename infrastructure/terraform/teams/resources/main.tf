6a2285b0eb3673f373d519272bf6641ee1e1ad9d
Esteban Cristóbal <ecristobalr@gmail.com>
2025-07-04 22:01:01 +0200

    fix(infrastructure): Added staging environment ID through environment variable

--- infrastructure/terraform/teams/resources/main.tf
+++ infrastructure/terraform/teams/resources/main.tf
@@ -60,25 +60,27 @@ resource "github_actions_variable" "terraform-workspace" {
   value         = tfe_workspace.workspace.name
 }
 
-data "confluent_environment" "staging" {
-  display_name = "staging"
+variable "staging_id" {
+  type        = string
+  nullable    = false
+  description = "Staging environment ID"
 }
 
 data "confluent_kafka_cluster" "staging" {
   display_name = "poc_kafka_cluster"
 
   environment {
-    id = data.confluent_environment.staging.id
+    id = var.staging_id
   }
 }
 
 resource "confluent_service_account" "team-admin" {
-  display_name = "${var.name}-${data.confluent_environment.staging.display_name}"
-  description  = "Service Account for team ${var.name} in ${data.confluent_environment.staging.display_name}"
+  display_name = "${var.name}-staging"
+  description  = "Service Account for team ${var.name} in staging"
 }
 
 resource "confluent_api_key" "team-admin" {
-  display_name = "${var.name}-${data.confluent_environment.staging.display_name}"
+  display_name = "${var.name}-staging"
   description  = "API Key for ${confluent_service_account.team-admin.display_name} service account"
 
   owner {
