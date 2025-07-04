aa13264bfa3a5bf560fcf95e8e4c2dd7dcc4933e
Esteban Cristóbal <ecristobalr@gmail.com>
2025-07-04 16:55:32 +0200

    feat(infrastructure): Added variables in GitHub Actions to define Terraform organization and workspace names, and created empty team workspace in Terraform Cloud

--- infrastructure/terraform/teams/resources/main.tf
+++ infrastructure/terraform/teams/resources/main.tf
@@ -33,8 +33,29 @@ data "github_repository" "team" {
   full_name = var.repository
 }
 
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "terraform-token" {
   repository      = data.github_repository.team.name
   secret_name     = "TF_TOKEN"
   plaintext_value = tfe_team_token.github-actions.token
+}
+
+resource "github_actions_variable" "terraform-organization" {
+  repository    = data.github_repository.team.name
+  variable_name = "TF_ORGANIZATION"
+  value         = "sagittec"
+}
+
+resource "tfe_workspace" "workspace" {
+  name         = "workspace-${var.name}"
+  organization = data.tfe_organization.sagittec.name
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "github_actions_variable" "terraform-workspace" {
+  repository    = data.github_repository.team.name
+  variable_name = "TF_WORKSPACE"
+  value         = tfe_workspace.workspace.name
 }
