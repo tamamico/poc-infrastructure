e5ec7583894c6647a04d007e6a9afe3e05f5ab40
Esteban Cristóbal <ecristobalr@gmail.com>
2025-07-05 22:12:45 +0200

    fix(infrastructure): Added API key to teams workspace

--- infrastructure/confluent/environment/main.tf
+++ infrastructure/confluent/environment/main.tf
@@ -35,10 +35,35 @@ resource "confluent_service_account" "staging-admin" {
   description  = "Staging admin service account"
 }
 
+resource "confluent_api_key" "staging-admin" {
+  display_name = "staging-admin"
+  description  = "API key for staging admin service account"
+
+  owner {
+    id          = confluent_service_account.staging-admin.id
+    api_version = confluent_service_account.staging-admin.api_version
+    kind        = confluent_service_account.staging-admin.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.poc.id
+    api_version = confluent_kafka_cluster.poc.api_version
+    kind        = confluent_kafka_cluster.poc.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "confluent_role_binding" "staging-admin-cluster" {
   principal   = "User:${confluent_service_account.staging-admin.id}"
-  role_name   = "CloudClusterAdmin"
-  crn_pattern = confluent_kafka_cluster.poc.rbac_crn
+  role_name   = "EnvironmentAdmin"
+  crn_pattern = confluent_environment.staging.id
 }
 
 data "confluent_organization" "sagittec" {}
@@ -53,4 +78,30 @@ resource "confluent_role_binding" "staging-admin-key" {
   principal   = "User:${confluent_service_account.staging-admin.id}"
   role_name   = "ResourceKeyAdmin"
   crn_pattern = data.confluent_organization.sagittec.resource_name
+}
+
+data "tfe_organization" "sagittec" {
+  name = "sagittec"
+}
+
+data "tfe_workspace" "teams" {
+  name         = "terraform-teams"
+  organization = data.tfe_organization.sagittec.name
+}
+
+resource "tfe_variable" "terraform-api-key" {
+  key          = "CONFLUENT_CLOUD_API_KEY"
+  value        = confluent_api_key.staging-admin.id
+  category     = "env"
+  description  = "Confluent Cloud API key"
+  workspace_id = data.tfe_workspace.teams.id
+}
+
+resource "tfe_variable" "terraform-api-secret" {
+  key          = "CONFLUENT_CLOUD_API_SECRET"
+  value        = confluent_api_key.staging-admin.secret
+  category     = "env"
+  sensitive    = true
+  description  = "Confluent Cloud API secret"
+  workspace_id = data.tfe_workspace.teams.id
 }
